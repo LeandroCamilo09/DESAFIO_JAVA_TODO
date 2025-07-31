82f8340661ad600b1e6700b30f4b47e4023bae13
Leandro Camilo <leandrocamilo09@gmail.com>
2025-07-31 19:50:44 -0300

    iniciando todo

---
+++ ToDo/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("             TODO LIST");
+        System.out.println("====================================");
+        System.out.println();
+        System.out.println("[1] Exibir lista de tarefas.");
+        System.out.println("[2] Adiconar tarefas.");
+        System.out.println("[3] Marcar como concluido.");
+        System.out.println("[4] Remover tarefa.");
+
+
+
+    }
+}
