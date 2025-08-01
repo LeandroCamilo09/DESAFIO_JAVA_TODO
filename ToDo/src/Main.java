48608ed20c2beffda7c7f4dd40d77e4cf96bff58
Leandro Camilo <leandrocamilo09@gmail.com>
2025-07-31 21:01:58 -0300

    construindo menu

--- ToDo/src/Main.java
+++ ToDo/src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import controlers.Menu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("====================================");
-        System.out.println("             TODO LIST");
-        System.out.println("====================================");
-        System.out.println();
-        System.out.println("[1] Exibir lista de tarefas.");
-        System.out.println("[2] Adiconar tarefas.");
-        System.out.println("[3] Marcar como concluido.");
-        System.out.println("[4] Remover tarefa.");
+
+        Menu MenuToDo = new Menu();
+        MenuToDo.exibirMenu();
 
 
 
---
+++ ToDo/src/controlers/Menu.java
@@ -0,0 +1,35 @@
+package controlers;
+import java.util.Scanner;
+public class Menu {
+    private Scanner scanner;
+    private int requisicaoMenu;
+
+
+    public Menu() {
+        this.scanner = new Scanner(System.in);
+        this.requisicaoMenu = requisicaoMenu;
+    }
+
+    public void exibirMenu(){
+        System.out.println("====================================");
+        System.out.println("             TODO LIST");
+        System.out.println("====================================");
+        System.out.println();
+        System.out.println("[1] Exibir lista de tarefas.");
+        System.out.println("[2] Adiconar tarefas.");
+        System.out.println("[3] Marcar como concluido.");
+        System.out.println("[4] Remover tarefa.");
+        System.out.println();
+        System.out.println("Escolha uma opção: ");
+        setRequisicaoMenu(scanner.nextInt());
+        System.out.println("Você digitou: " + getRequisicaoMenu());
+    }
+
+    public int getRequisicaoMenu() {
+        return requisicaoMenu;
+    }
+
+    public void setRequisicaoMenu(int requisicaoMenu) {
+        this.requisicaoMenu = requisicaoMenu;
+    }
+}
